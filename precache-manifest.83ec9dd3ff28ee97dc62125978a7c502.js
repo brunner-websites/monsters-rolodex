18f4cc4bdbd4095343098a430b1d9922590bf70f
sandro <sandro@brunner-websites.com>
2020-04-02 02:42:55 +0200

    Updates

---
+++ precache-manifest.83ec9dd3ff28ee97dc62125978a7c502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadede4150962433498f762d8ff7548c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d190affdca058fd2409",
+    "url": "/monsters-rolodex/static/css/main.6652af34.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d190affdca058fd2409",
+    "url": "/monsters-rolodex/static/js/main.dda2234e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
